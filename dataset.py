7344dab3106ae4a2a9fdac34867ffdb25ce8d9a5
Gleb Tutik <tutikgv@gmail.com>
2022-11-27 17:14:03 +0600

    Добавлен oversampling

--- dataset.py
+++ dataset.py
@@ -9,9 +9,33 @@ from torch.nn.functional import one_hot
 
 
 class CrimeanPlantsDataset(Dataset):
-    def __init__(self, root_dir: str, csv_file: str, transform=None):
+    def __init__(self, root_dir: str, csv_file: str, oversampling: bool = False, transform=None,):
         self.root_dir = root_dir
-        self.data_csv = pd.read_csv(csv_file)
+
+        if oversampling:
+            df_csv = pd.read_csv(csv_file)
+            max_class_length = df_csv["label"].value_counts().max()
+            class_labels = df_csv["label"].unique()
+
+            data_csv = pd.DataFrame()
+            for label in class_labels:
+                current_class = df_csv.loc[df_csv["label"] == label]
+
+                add_samples_number = max_class_length - len(current_class)
+
+                current_class_oversampled = pd.concat(
+                    [
+                        current_class,
+                        current_class.sample(add_samples_number, replace=True)
+                    ]
+                )
+
+                data_csv = pd.concat([data_csv, current_class_oversampled])
+
+            self.data_csv = data_csv
+        else:
+            self.data_csv = pd.read_csv(csv_file)
+
         self.transform = transform
 
     def __len__(self):
@@ -33,9 +57,8 @@ class CrimeanPlantsDataset(Dataset):
 
 def dataset_test():
     root_dir = "./data/dataset/"
-    csv_file = "./data/dataset/test_labels.csv"
-    dataset = CrimeanPlantsDataset(root_dir=root_dir, csv_file=csv_file)
-    print("ok")
+    csv_file = "./data/dataset/train_labels.csv"
+    dataset = CrimeanPlantsDataset(root_dir=root_dir, csv_file=csv_file, oversampling=True)
 
 
 if __name__ == "__main__":
