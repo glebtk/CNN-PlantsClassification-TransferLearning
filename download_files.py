2a96854b21f27bb7fc50e8f703e8cfc894729cbd
Gleb Tutik <tutikgv@gmail.com>
2022-12-05 17:50:53 +0600

    Добавлена загрузка чекпоинта (accuracy on test 0.99)

--- download_files.py
+++ download_files.py
@@ -5,7 +5,7 @@ import urllib.request
 
 # Предустановки
 LOAD_DATASET = True         # Загрузить датасет для обучения
-# LOAD_CHECKPOINT = True      # Загрузить чекпоинт с предобученными весами
+LOAD_CHECKPOINT = True      # Загрузить чекпоинт с предобученными весами
 
 
 def download_and_unzip(url, path, name):
@@ -28,15 +28,15 @@ def download_files():
 
         print("=> Датасет загружен!")
 
-    # if LOAD_CHECKPOINT:
-    #     url = "https://gitlab.com/glebtutik/european_to_asian_files/-/raw/main/checkpoints/checkpoints.zip"
-    #     path = "checkpoints"
-    #     file_name = "checkpoints.zip"
-    #
-    #     download_and_unzip(url, path, file_name)
-    #     os.remove(os.path.join(path, file_name))
-    #
-    #     print("=> Чекпоинт загружен!")
+    if LOAD_CHECKPOINT:
+        url = "https://gitlab.com/glebtutik/crimean_plants_classification_files/-/raw/main/checkpoints/plants_model_checkpoint.zip"
+        path = "checkpoints"
+        file_name = "checkpoints.zip"
+
+        download_and_unzip(url, path, file_name)
+        os.remove(os.path.join(path, file_name))
+
+        print("=> Чекпоинт загружен!")
 
 
 if __name__ == "__main__":
